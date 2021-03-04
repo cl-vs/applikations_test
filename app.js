43fc5159fea4ce7d73dd64cf37e6e73040dfe270
Claudia von Semkov <claudia.von.semkov@prevas.se>
2021-03-04 16:49:49 +0100

    server test

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
-for(let i = 0; i < 1000000; i++){
-    console.log(i)
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
