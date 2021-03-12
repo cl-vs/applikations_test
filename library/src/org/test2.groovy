a2b174e76112780c50d1e0c8bbad66624edea4d2
cl-vs <claudia.von.semkov@prevas.se>
2021-03-12 14:19:30 +0100

    lk

--- library/src/org/test2.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-// vars/YourStepName.groovy
-
-def call() {
-    print("hej test grrovy test 2")
-    // Do something here...
-}
--- library/vars/test.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-// vars/YourStepName.groovy
-
-def call() {
-    print("hej test grrovy test")
-    // Do something here...
-}
