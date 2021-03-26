98aba90e600415dad0fb92560d15b67b0d6a0117
cl-vs <claudia.von.semkov@prevas.se>
2021-03-26 11:33:08 +0100

    test

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print("on start")
+
 @app.route('/')
 def hello():
+    print("request received")
     return 'Hello'
-
-if __name__ == '__main__':
-    app.run()
